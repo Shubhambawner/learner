b0ed8b2f1ae9eb7126fd3dd6c7075b5c00dc14b4
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2021-12-12 03:12:16 +0530

    import statement

--- Chess/chess.js
+++ Chess/chess.js
@@ -1,3 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+
 class Square extends React.Component {
   //console.log(props.value)
   #x = this.props.value[1];
