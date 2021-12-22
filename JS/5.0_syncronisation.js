3b837fc65453a5f0d990e0d2a565101b44998a8d
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2021-12-22 12:43:22 +0530

    syncronisation in JS

---
+++ JS/5.0_syncronisation.js
@@ -0,0 +1,15 @@
+/**
+ * ASYNCRONUS FUNCTIONS:
+ * functions that run in background with other thread being asigned
+ * are called asyncronus functions.
+ * these are not in sync with the other js code
+ * ex:
+ * setInterval() , setTimeOut() ...
+ * 
+ * SYNCRONUS FUNCTIONS:
+ * functions for whose execution the JS code execution waits, is called SYNCRONUS
+ * ex: forEach() , 
+ * by default any function we create is syncronuous
+ * 
+ * Usefull to understand multi threading in js
+ */
