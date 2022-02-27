747ffe2251d7f58efe44b0ed1afd0e6962e1ff99
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-02-28 00:33:20 +0530

    form events DOM manipulations

--- Dev/JS/7_event_listners.js
+++ Dev/JS/7_event_listners.js
@@ -27,6 +27,15 @@
 
 /**
  * form events...
+ * submit //when the form is submitted (not for file upload) 
+ * reset //when the form is reset to initial state (not used)
+ * focus //when the form is focused on (not on the input)
+ * blur //when the form is blurred (lost focus)
+ * select //when the form is selected (for textarea)
+ * change //when the form is changed (for input)
+ * input //when the form is input (for input)
+ * invalid //when the form is invalid (for input)
+ * 
  */
 
 e1.addEventListener('click', function ho() {
