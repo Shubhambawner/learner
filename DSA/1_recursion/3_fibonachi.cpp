cd156bd2e0fad638f29e86bb73cc891d9a54282e
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-08-02 21:24:27 +0530

    We should always pass the referance to data-structure in the function instead of creating the global datastructure. this is because

--- DSA/1_recursion/3_fibonachi.cpp
+++ DSA/1_recursion/3_fibonachi.cpp
@@ -35,34 +35,11 @@ int fibonacci(int n)
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-vector<long long> d = {0,1};
-long long fibonacci_dp(int n ){
-    if(n<d.size()) return d[n];
-    else if(n==d.size()){
-        d.push_back(d[n-1]+d[n-2]);
-        return d[n];
-    }else{
-        return fibonacci_dp(n-1)+fibonacci_dp(n-2);
-    }
-}
-
-ll last_second=0,last=1,N=2;
-ll fibonacci_dp_space_optimised(int n){
-    if(N==n){
-        N++;
-        last = last+last_second;
-        last_second = last - last_second;
-        return last;
-    }else if(n==N-1)return last;
-    else if(n==N-2)return last_second;
-    else return fibonacci_dp_space_optimised(n-1)+fibonacci_dp_space_optimised(n-2);
-}
-
 int main()
 {
-    // cout<<palindrome("MADAM");
-    // cout<<flip("1234567890");
-    // cout<<fibonacci(20)<<endl;
-    cout<<fibonacci_dp(45)<<"\n";
-    cout<<fibonacci_dp_space_optimised(45);
+    cout<<palindrome("MADAM");
+    cout<<flip("1234567890");
+    cout<<fibonacci(20)<<endl;
+    // cout<<fibonacci_dp(45)<<"\n";
+    // cout<<fibonacci_dp_space_optimised(45);
 }
